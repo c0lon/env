1ab6df126e78bd1a0dfee3850e0e86517ca43780
Collin Barth <collin@qintel.com>
2017-02-28 11:21:55 -0500

    ugh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# setup my development environment
+#
+# sets up vim
+# sets up tmux
+# sets up pyenv + pyenv-virtualenv TODO
+# 
+# TODO
+# figure out relative symbolic links
+
+# vim
+#ln -s ./vim $HOME/.vim
+#ln -s ./vim/vimrc $HOME/.vimrc
+
+# tmux
+#ln -s tmux.conf $HOME/.tmux.conf
+
+# pyenv
+# git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+# echo 'export PYENV_ROOT=$HOME/.pyenv' >> ~/.bashrc
+# echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+# echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+# pyenv-virtualenv
+# git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugsin/pyenv-virtualenv
+# echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
